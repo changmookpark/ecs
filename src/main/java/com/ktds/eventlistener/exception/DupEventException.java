937b57e49a3d18fa6f63a744cc8d618cef8066bd
me33x3 <me33x3@gmail.com>
2024-01-22 23:24:42 +0900

    중복 이벤트 에러 코드 추가

--- src/main/java/com/ktds/eventlistener/exception/DupEventException.java
+++ src/main/java/com/ktds/eventlistener/exception/DupEventException.java
@@ -2,7 +2,38 @@ package com.ktds.eventlistener.exception;
 
 public class DupEventException extends Exception {
 
+    public static final int DUP_EVENT = 1;
+    public static final int DUP_EVENT_IN_PROGRESS = 2;
+    public static final int DUP_EVENT_PROGRESSED = 3;
+
+    private int code;
+
     public DupEventException(String message) {
         super(message);
+    }
+
+    public DupEventException(int code) {
+
+        super(getMessage(code));
+        this.code = code;
+    }
+
+    private static String getMessage(int code) {
+
+        String message = "";
+
+        switch(code) {
+            case DUP_EVENT:
+                message = "Dup Evnet skip";
+                break;
+            case DUP_EVENT_IN_PROGRESS:
+                message = "Dup Evnet skip (In Progress)";
+                break;
+            case DUP_EVENT_PROGRESSED:
+                message = "Dup Evnet skip (Progressed)";
+                break;
+        }
+
+        return message;
     }
 }
