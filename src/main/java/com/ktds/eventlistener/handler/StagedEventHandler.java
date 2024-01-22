c2534210f22425787652b8c0cfc6f5d2b6387df7
me33x3 <me33x3@gmail.com>
2024-01-22 23:36:02 +0900

    Staged Event 처리 로직 개발

--- src/main/java/com/ktds/eventlistener/handler/StagedEventHandler.java
+++ src/main/java/com/ktds/eventlistener/handler/StagedEventHandler.java
@@ -1,13 +1,75 @@
 package com.ktds.eventlistener.handler;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ktds.eventlistener.exception.DupEventException;
+import com.ktds.eventlistener.model.RefinedEvent;
 import com.ktds.eventlistener.model.StagedEvent;
+import com.ktds.eventlistener.service.StagedEventService;
+
+import jakarta.transaction.Transactional;
 
 @Component
 public class StagedEventHandler implements EventHandler<StagedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(StagedEventHandler.class);
+
+    @Autowired
+    StagedEventService service;
     
+    @Transactional
     public void processEvent(StagedEvent event) {
         
+        logger.info(String.format("(%s) Starting to handle staged event data... [%d]", event.getEventId(), ("1".equals(event.getEventType()) ? 1 : 2)));
+        Optional<RefinedEvent> optional = Optional.empty();
+
+        try {
+
+            logger.info(String.format("(%s) Check for duplicate event...", event.getEventId()));
+            optional = service.findDupEvent(event);
+
+            if (optional.isPresent()) {
+                throw new DupEventException(DupEventException.DUP_EVENT);
+            }
+
+            if ("1".equals(event.getEventType())) {
+
+                logger.info(String.format("(%s) Check for duplicate events in progress...", event.getEventId()));
+                optional = service.findInProgressEvent(event);
+
+                if (optional.isPresent()) {
+                    throw new DupEventException(DupEventException.DUP_EVENT_IN_PROGRESS);
+                }
+            } else {
+
+                optional = service.findProgressedEvent(event);
+
+                if (optional.isPresent()) {
+                    if ("2".equals(optional.get().getEventType())) {
+                        throw new DupEventException(DupEventException.DUP_EVENT_PROGRESSED);
+                    }
+                } else {
+                    throw new Exception();
+                }
+            }
+        } catch (DupEventException ex) {
+        
+            RefinedEvent refinedEvent = optional.get();
+            String passMessage = ex.getMessage();
+
+            logger.info(String.format("(%s) %s", event.getEventId(), passMessage));
+        } catch (Exception ex) {
+
+            String passMessage = String.format("Request Error skip (%s)", ex.getClass().getSimpleName());
+            logger.info(String.format("(%s) %s", event.getEventId(), passMessage));
+        } finally {
+
+            logger.info(String.format("(%s) Finished handling staged event data...", event.getEventId()));
+        }
     }
 }
