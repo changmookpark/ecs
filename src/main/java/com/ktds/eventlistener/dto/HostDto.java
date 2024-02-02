6d2038795a8ad0a78040c295440a9e03f49ba2e4
me33x3 <me33x3@gmail.com>
2024-02-02 14:57:12 +0900

    webhook 전송을 위한 dto 모델 추가

---
+++ src/main/java/com/ktds/eventlistener/dto/HostDto.java
@@ -0,0 +1,17 @@
+package com.ktds.eventlistener.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HostDto {
+    
+    private String connectionInfo;
+
+    private String id;
+
+    private String visibleName;
+
+    private String name;
+}
